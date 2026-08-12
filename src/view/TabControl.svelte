7f5b8203350aa025ca24e59ba617c3fef0295b99
mrdkprj <rockfield.d.m@gmail.com>
2026-08-12 18:25:35 +0900

    tabcontrol prevent focus move

--- src/view/TabControl.svelte
+++ src/view/TabControl.svelte
@@ -14,7 +14,14 @@
         tab.scrollLeft = tabs.scrollLeft;
     });
 
-    const closeTab = async (label: string) => {
+    const onCloseButtonMousedown = (e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+    };
+
+    const closeTab = async (e: MouseEvent, label: string) => {
+        e.preventDefault();
+        e.stopPropagation();
         await ipc.sendTo(label, "tab_event", { name: "close" });
     };
 
@@ -143,7 +150,7 @@
             animate:flip={{ duration: tabState.dragging ? 400 : 0 }}
         >
             <div class="tab-title" title={tab.title}>{tab.title}</div>
-            <div class="tab-close-btn" onclick={() => closeTab(tab.label)} onkeydown={() => {}} role="button" tabindex="-1">
+            <div class="tab-close-btn" onclick={(e) => closeTab(e, tab.label)} onmousedown={onCloseButtonMousedown} onkeydown={() => {}} role="button" tabindex="-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                     <path
                         d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"
